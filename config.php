d162b7bc989a3db5e878c78edd308825ec6edd69
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-09-29 17:32:02 +0400

    update database

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-$dbname = "uowd"; /* Database name */
+$host = "127.0.01"; /* Host name */
+$user = "mariadb"; /* User */
+$password = "mariadb"; /* Password */
+$dbname = "mariadb"; /* Database name */
 
-$con = mysqli_connect($host, $user, $password,$dbname);
+$con = new mysqli_connect($host, $user, $password, $dbname);
 // Check connection
 if (!$con) {
  die("Connection failed: " . mysqli_connect_error());
